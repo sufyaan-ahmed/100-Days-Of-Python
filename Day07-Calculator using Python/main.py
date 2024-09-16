23e42d85174466fdd5b1821c1d87922aa0cef4bf
sufyaan-ahmed <sufiahmed.dev@gmail.com>
2024-09-16 22:05:13 +0500

    first commit

---
+++ Day07-Calculator using Python/main.py
@@ -0,0 +1,21 @@
+# operators
+# python has different types of operators for different operation.
+# To create a calculator we require Arithmatic operators.
+
+print(5 + 5)  # Addition
+print(5 - 5)  # Subtraction
+print(5 * 5)  # Multiplicaton
+print(5 / 5)  # Division
+print(5 % 5)  # Modulus
+print(5 // 5) # Floor Division
+print(5 ** 5) # Exponential
+
+# Calculator (simplified)
+
+num1 = (5)
+num2 = (10)
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
---
+++ Day07-Calculator using Python/readme.md
@@ -0,0 +1,5 @@
+<h1>Calculator using Python</h1>
+
+<p>Day07-We will learn about differnet operators that are used in<br>
+Pyton to perform different operators and a very simple calculatar using<br>
+variables and operators.</p>
